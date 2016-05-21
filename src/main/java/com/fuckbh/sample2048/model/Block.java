a8c0f74a20af2e16f8e940a8fbfef94e8d35f4e9
Jeon Ji Hoon <jhjeon0728@naver.com>
2016-05-21 14:41:49 +0900

    블록 클리스 정의 closed #2

---
+++ src/main/java/com/fuckbh/sample2048/model/Block.java
@@ -0,0 +1,14 @@
+package com.fuckbh.sample2048.model;
+
+public class Block {
+	
+	private int value;
+	
+	public int getValue() {
+		return this.value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
