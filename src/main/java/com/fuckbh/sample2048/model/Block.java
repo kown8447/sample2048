417a7f3fd63a5f4693a3cba2892c7d9ed5422cdc
Jeon Ji Hoon <jhjeon0728@naver.com>
2016-05-21 15:25:01 +0900

    테스트 돌아가도록 소스 작성

--- src/main/java/com/fuckbh/sample2048/model/Block.java
+++ src/main/java/com/fuckbh/sample2048/model/Block.java
@@ -3,6 +3,13 @@ package com.fuckbh.sample2048.model;
 public class Block {
 	
 	private int value;
+	private Point point;
+	
+	public Block(){}
+	public Block(int value, Point point) {
+		this.value = value;
+		this.point = point;
+	}
 	
 	public int getValue() {
 		return this.value;
@@ -10,5 +17,12 @@ public class Block {
 	
 	public void setValue(int value) {
 		this.value = value;
+	}
+	
+	public Point getPoint() {
+		return point;
+	}
+	public void setPoint(Point point) {
+		this.point = point;
 	}
 }
